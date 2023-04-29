cd3ebde9474bffb746cd4e2fd68c0bf9e2f9a8e9
Adarsh Tiwari <adarsh.tiwari@tensorgo.com>
2023-04-29 14:45:23 +0530

    commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,11 @@ import { motion } from 'framer-motion'
 import {fadeIn} from "../utils/motion"
 import Tilt from "react-tilt"
 import { useNavigate } from 'react-router-dom'
+// import { Buffer } from "buffer";
+// import { PublicKey } from '@solana/web3.js';
+// import * as web3 from '@solana/web3.js';
+
+window.Buffer = Buffer;
 
 const Button = ({ title }) => {
   console.log(title)
@@ -20,7 +25,8 @@ const navigate=useNavigate()
             speed:450
           }}
           className='bg-tertiary rounded-[20px] py-2 px-12 min-h-[20px] flex-justify-evenly items-center flex-col'
-          onClick={()=> {
+          onClick={() => {
+            if(title !== 'DonateNow')
             navigate('/create/fundraiser/types')
           }}
         >
